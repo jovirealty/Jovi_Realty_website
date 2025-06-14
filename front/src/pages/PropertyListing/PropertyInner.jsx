b2eeea836e2cce93d2c37621b22eaf763b53c8e3
shabbirumrali <shabbirumrali@gmail.com>
2025-06-14 10:04:33 +0530

    attached property details via listingKey

--- front/src/pages/PropertyListing/PropertyInner.jsx
+++ front/src/pages/PropertyListing/PropertyInner.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import useBridgeApi from "../../hooks/useBridgeApi";
+
 import Header from "../../components/shared/Sections/Header/header";
 import Footer from "../../components/shared/Sections/Footer/footer";
 import GallerySection from "../../components/shared/Sections/Gallery/Gallery";
@@ -6,29 +9,32 @@ import PropertyDetails from "../../components/shared/Sections/PropertyDetails/Pr
 import PropertyGrid from "../../components/shared/Sections/PropertyGrid/PropertyGrid";
 import CTASection from "../../components/shared/Sections/CTASection/CTASection";
 
-import media1 from "./../../assets/Images/property-img-1.png";
-import media2 from "./../../assets/Images/property-img-2.png";
-import media3 from "./../../assets/Images/property-img-3.png";
-import media4 from "./../../assets/Images/property-img-4.png";
-import media5 from "./../../assets/Images/property-img-5.png";
-
-const mediaItems = [
-    media1,
-    media2,
-    media3,
-    media4,
-    media5,
-    media3
-];
-
 const PropertyInner = () => {
+  const { listingKey } = useParams();
+
+  const { data: property, error, loading } = useBridgeApi(
+    `/Property(${listingKey})`,
+    {},
+    false,
+  );
+
+  if(property) {
+    console.log("property Inner values: ", property);
+  }
+  
   return (
     <div className="property-listing property-listing-rent">
       <Header />
-      <GallerySection media={mediaItems} />
-      <PropertyDetails />
-      <PropertyGrid statusFilter="For Sale" title="Explore Similar Property" totalItems={4} />
-      <CTASection />
+      {property ? (
+        <>
+          <GallerySection propertyMedia={property.Media} />
+          <PropertyDetails propertyInfo={property} />
+          <PropertyGrid statusFilter="For Sale" title="Explore Similar Property" totalItems={4} />
+          <CTASection />
+        </>
+      ) : (
+        <div>Loading</div>
+      )}
       <Footer />
     </div>
   );
