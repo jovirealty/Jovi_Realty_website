c7e3f8f5bdd1cfcdfe830c913498382c1c02cc4d
shabbirumrali <shabbirumrali@gmail.com>
2025-06-22 19:52:07 +0530

    image not available issue fixed

--- front/src/components/shared/Elements/PropertyCard/PropertyCard.jsx
+++ front/src/components/shared/Elements/PropertyCard/PropertyCard.jsx
@@ -10,6 +10,7 @@ import squareFeetIcon from "./../../../../assets/Images/square-ft-icon.png";
 const PropertyCard = ({ property, status }) => {
   const officeMlsId = property.ListOfficeMlsId;
   const officeName = useOfficeNameCached(officeMlsId);
+  console.log("property card: ", property)
 
   const buildAddress = (property) => {
     const sanitize = (value) => {
@@ -37,7 +38,29 @@ const PropertyCard = ({ property, status }) => {
     <div className="property-card position-relative">
       <div className="property-card-img">
         <span className="badge">{status}</span>
-        <img src={Object.keys(property) ? (property?.Media ? property?.Media[0]?.MediaURL : property?.image) : property?.image} alt="Property Image" />
+        {property?.Media && property.Media.length > 0 && property.Media[0]?.MediaURL ? (
+          <img
+            src={property.Media[0].MediaURL}
+            alt="Property Image"
+            style={{ width: "100%", height: "200px", objectFit: "cover", background: "#e6e7e9" }}
+          />
+        ) : (
+          <div
+            style={{
+              width: "100%",
+              height: "200px",
+              background: "#e6e7e9",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              color: "#888",
+              fontWeight: "bold",
+              fontSize: "1.1rem"
+            }}
+          >
+            No Image Available
+          </div>
+        )}
       </div>
       <div className="property-card-body">
         <h4 className="property-price">${property.ListPrice?.toLocaleString()}</h4>
