5b2daf99cccd1ed92af8c74c953fcb34bbdf5a27
Shabbir Umrali <hello@jovirealty.com>
2025-07-10 11:11:19 +0530

    ABOUTPAGE - content updated

--- src/components/About/Banner/Banner.jsx
+++ src/components/About/Banner/Banner.jsx
@@ -5,8 +5,8 @@ const Banner = () => {
     return (
         <div className="about-banner">
             <div className="about-banner-content">
-                <h1 className="main-h1">About Us</h1>
-                <p className="banner-subtitle">Jovi: Your Shortcut to the Best Real Estate Deals</p>
+                <h1 className="main-h1">Your Premier Greater Vancouver Real Estate Brokerage</h1>
+                <p className="banner-subtitle">Guiding You Through Every Transaction with Expertise</p>
             </div>
         </div>
     );
