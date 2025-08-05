265214dbc61209847ac837b3a73d858a772090bf
Shabbir Umrali <hello@jovirealty.com>
2025-08-05 17:21:33 +0530

    Updated - the values from useBridge to useApis and redesign the card

--- src/components/shared/Elements/AgentCard/AgentCard.jsx
+++ src/components/shared/Elements/AgentCard/AgentCard.jsx
@@ -1,28 +1,48 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import { FiMapPin } from "react-icons/fi";
 import getInitials from "../../../../utils/getInitials";
+import ajMohamedImage from "../../../../assets/Images/aj-mohamed.png"
+
+// CSS
 import "./AgentCard.css";
 
 const AgentCard = ({ agent }) => {
     const initials = getInitials(agent);
     
     return (
-        <Link to={`/find-an-agent/agent-profile/${agent.MemberKey}`} className="agent-card position-relative d-flex flex-column justify-content-end">
-            <div
-                className="agent-image-initials position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center justify-content-center"
-                style={{
-                    fontWeight: "bold",
-                    fontSize: "2.5rem",
-                    borderRadius: "50%",
-                }}
-            >
-                {initials}
+        <div className="agent-card position-relative d-flex flex-column justify-content-end">
+            <div className="agent-image-initials position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center justify-content-center" >
+                <img 
+                    src={ajMohamedImage}
+                    className="agent-image position-absolute top-0 start-0 w-100 h-100" 
+                />
             </div>
+
             <div className="agent-details bg-white position-relative">
-                <h4 className="agent-name mb-1">{agent.MemberFullName}</h4>
-                <p className="agent-designation mb-0">{agent.designation}</p>
+                <div className="d-flex justify-content-between">
+                    <div className="agent-firm d-flex gap-1 align-items-center">
+                        <p className="text-primary text-decoration-none mb-0 font-bold">Jovi Realty Inc.</p>
+                    </div>
+                    <div className="agent-location d-flex gap-1 align-items-center">
+                        <FiMapPin />
+                        <p className="text-decoration-none mb-0">Location</p>
+                    </div>
+                </div>
+                <h4 className="agent-name mt-3">{agent.knownAs && agent.knownAs.trim() !== '' ? agent.knownAs : agent.fullName}</h4>
+                <div className="d-flex justify-content-between align-items-center">
+                    <p className="agent-designation mb-0">Sales {agent.licensedAs}</p>
+                    <span className="agent-license-id">#{agent.licenseNumber}</span>  
+                </div>
             </div>
-        </Link>
+
+            <Link
+                to={`/find-an-agent/agent-profile/${agent.mlsId}`} 
+                className="agent-view-profile-button btn-primary w-100 m-0 text-decoration-none text-center" 
+            >
+                View Profile
+            </Link>
+        </div>
     );
 };
 
